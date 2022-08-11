ff3c753810d165c7ddfa49427c484dc35b603392
ecln30 <ecln30@gmail.com>
2022-08-12 01:02:21 +0900

    changes cart, statecontex etc...

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -23,7 +23,7 @@ import {urlFor} from '../lib/client'
 
 const Cart = () => {
     const cartRef = useRef()
-    const { totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext();
+    const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity,onRemove} = useStateContext();
 
     return(
              <div className='cart-wrapper' ref={cartRef}>
@@ -67,16 +67,45 @@ const Cart = () => {
                             <div className='flex bottom'>
                               <div>
                               <p className="quantity-desc">
-                             <span className="minus" onClick=""><AiOutlineMinus /></span>
-                             <span className="num"> 0 </span>
-                             <span className="plus" onClick=""><AiOutlinePlus /></span>
+                             <span className="minus" onClick={() =>
+                              toggleCartItemQuantity(item._id, 'dec')
+                             }><AiOutlineMinus /></span>
+                             <span className="num"> {item.quantity} </span>
+                             <span className="plus" onClick={() =>
+                              toggleCartItemQuantity(item._id, 'inc')
+                             }><AiOutlinePlus /></span>
                              </p>
                               </div>
+                              <buttom 
+                                 type="buttom"
+                                 className="remove-item"
+                                 onClick={() => onRemove(item)}
+                              >
+                                <TiDeleteOutline />
+                              </buttom>
                             </div>
                           </div>
                         </div>
                     ))}
                 </div>
+                 {cartItems.length >= 1 && (
+                     <div className='cart-bottom'>
+                       <div className='total'>
+                         <h3>Subtotal: </h3>
+                         <h3> ${totalPrice} </h3>
+                       </div>
+                       <div className='btn-container'>
+                          <button 
+                          type="button" 
+                          className='btn'
+                          onClick="" 
+                          >
+                           pay with Stripe
+                          </button>
+                       </div>
+                     </div>
+                 ) }
+
               </div>
             </div>
         )
