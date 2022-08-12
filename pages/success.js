5726fc4d403ce3ef1ceb7bf42ce90579170840e4
ecln30 <ecln30@gmail.com>
2022-08-12 18:15:50 +0900

    commit

--- pages/success.js
+++ pages/success.js
@@ -17,7 +17,7 @@ import Link from "next/link"
 import {BsBagCheckFill} from 'react-icons/bs'
 import {useRouter} from 'next/router'
 
-import {useStateContect} from '../context/StateContext'
+import {useStateContext} from '../context/StateContext'
 
 function success() {
 
@@ -32,8 +32,21 @@ function success() {
         <p className="icon">
           <BsBagCheckFill />
         </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+            If you have any questions, please email
+           <a href="mailto: order@example.com" className="email">
+           order@example.com
+           </a>
+        </p>
+        <Link href="/">
+          <button type="button" width='300px' className="btn">
+             Continue Shopping
+          </button>
+        </Link>
     </div>
-
+      
     </div>
   )
 }
